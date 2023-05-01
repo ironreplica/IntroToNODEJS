7a29f52fb7443f2149d13c435cd23befb6289c2e
TREVOR CHILDS <trevorchilds8@gmail.com>
2023-05-01 13:29:29 -0600

    Signed-off-by: TREVOR CHILDS <trevorchilds8@gmail.com>

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+
+app.get("/", (req,res) => {
+    res.send("Hello World!");
+});
+
+app.listen(8080, () => {
+    console.log(`Test server running on port: ${PORT}`);
+});
